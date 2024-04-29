541a582bb7562173bd4004c8d500e0dce9bc26f6
xizhonghua <xizhonghua@gmail.com>
2024-04-28 19:39:46 -0700

    Add flags for run_video.py

--- run_video.py
+++ run_video.py
@@ -1,70 +1,76 @@
 #!python
 
+"""
+Usage:
+  ./run_video.py -i input.mp4 -o output.mp4
+
+"""
+import argparse
 import tensorflow as tf
 import cv2
 import numpy as np
 import sys
 from vidgear.gears import WriteGear
 
-args = sys.argv[1:]
+parser = argparse.ArgumentParser(description="Run video.")
+parser.add_argument("-i", "--input")
+parser.add_argument("-o", "--output")
+parser.add_argument("-v", "--visualize", default=False, action=argparse.BooleanOptionalAction)
+parser.add_argument("--height", default=720)
+args = parser.parse_args()
 
-input_video = args[0]
-output_video = args[1]
-SCALE=args[2] if len(args) > 2 else 2
+print(args)
+print(args.visualize)
 
-model = tf.keras.models.load_model('saved')
- 
+model = tf.keras.models.load_model("saved")
+
 def process_frame(lr):
-    lr = tf.expand_dims(lr, axis=0)
-    lr = tf.image.convert_image_dtype(lr, dtype=tf.float32, saturate=True)
-    sr = model(lr)
-    sr = tf.image.convert_image_dtype(sr, dtype=tf.uint8, saturate=True)
-    sr = tf.squeeze(sr, axis=0)
-    return sr.numpy()
+  lr = tf.expand_dims(lr, axis=0)
+  lr = tf.image.convert_image_dtype(lr, dtype=tf.float32, saturate=True)
+  sr = model(lr)
+  sr = tf.image.convert_image_dtype(sr, dtype=tf.uint8, saturate=True)
+  sr = tf.squeeze(sr, axis=0)
+  return sr.numpy()
 
-# Creating a VideoCapture object to read the video
-cap = cv2.VideoCapture(input_video)
+cap = cv2.VideoCapture(args.input)
 
 # get cap property 
-width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
+width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = cap.get(cv2.CAP_PROP_FPS)
 
-print(f'{width}*{height}, {fps=}')
+print(f"{width}*{height}, {fps=}")
 
-output_params = {"-vcodec":"libx264", "-crf": 15, "-preset": "fast", "-input_framerate":fps, "-r":fps, "-pix_fmt": "yuv420p"}
+output_params = {"-vcodec": "libx264", "-crf": 15, "-preset": "fast", "-input_framerate":fps, "-r":fps, "-pix_fmt": "yuv420p"}
 
-writer = WriteGear(output = output_video, compression_mode = True, logging = True, **output_params)
+writer = WriteGear(output = args.output, compression_mode = True, logging = True, **output_params)
  
-# Loop until the end of the video
 
-cv2.namedWindow("lr | sr", cv2.WINDOW_NORMAL) 
+if args.visualize:
+  cv2.namedWindow("lr | sr", cv2.WINDOW_NORMAL) 
 
 while (cap.isOpened()):
- 
-    # Capture frame-by-frame
-    ret, lr_frame = cap.read()
+  ret, lr_frame = cap.read()
 
-    if not ret: break
- 
-    sr_frame = process_frame(lr_frame)
+  if not ret: break
 
-    lr_frame_scaled = cv2.resize(lr_frame, (sr_frame.shape[1], sr_frame.shape[0]))
-    
-    frame = np.vstack((lr_frame_scaled, sr_frame))
+  sr_frame = process_frame(lr_frame)
 
-    # Display the resulting frame
-    cv2.imshow('lr | sr', frame)
+  preview_h = args.height
+  preview_w = int(sr_frame.shape[1] * preview_h * 1.0 / sr_frame.shape[0])
 
-    # output.write(sr_frame)
-    writer.write(sr_frame)
+  if args.visualize:
+    lr_preview = cv2.resize(lr_frame, (preview_w, preview_h))
+    sr_preview = cv2.resize(sr_frame, (preview_w, preview_h))
+    frame = np.vstack((lr_preview, sr_preview))
+    cv2.imshow("lr | sr", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+      break
+
+  writer.write(sr_frame)
  
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
- 
-# release the video capture object
 cap.release()
 writer.close()
 
-# Closes all the windows currently opened.
-cv2.destroyAllWindows()
+if args.visualize:
+  cv2.destroyAllWindows()
