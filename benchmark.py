a5d05260a3da9b2d847279d30612994f3996af60
xizhonghua <xizhonghua@gmail.com>
2024-04-29 17:28:01 -0700

    Add flags for benchmark script

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,23 @@
 #!python
 
+import argparse
 import tensorflow as tf
 import time
 
-model = tf.keras.models.load_model('saved')
-print(model.summary)
+parser = argparse.ArgumentParser(description="Benchmark Keras models.")
+parser.add_argument("-m", "--model", default="models/anime_v1_3x")
+parser.add_argument("--width", type=int, default=640)
+parser.add_argument("--height", type=int, default=360)
+args = parser.parse_args()
 
-input_image = tf.ones([1, 360, 640, 3])
+model = tf.keras.models.load_model(args.model)
+print(model.summary())
+
+input_image = tf.ones([1, args.height, args.width, 3])
 
 now = time.time()
 runs = 100
+print(f"Benchmark {input_image.shape} for {runs=}")
 for _ in range(runs):
     output_image = model(input_image)
 
